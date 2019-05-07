92ae0046dc1458ce606cb351824ee984f492a2c7
Musa <tamzid101@gmail.com>
2019-05-08 01:57:57 +0600

    basic defines are added to defines.js.

---
+++ defines.js
@@ -0,0 +1,9 @@
+
+mapboxgl.accessToken = "pk.eyJ1IjoiZ2lzZmVlZGJhY2siLCJhIjoiY2pwdWJjaXRuMGV6YjQyb2c2NDY3b29wdCJ9.54kPmhZkN3sfNjDNUDqwIg";
+
+// We save layer IDs here so we can refer to them later
+var buildingsLayerID = "dam-buildings";
+var highlightExtrusionLayerID = "dam-buildings-highlight-extrusion";
+var highlightShadowLayerD = "dam-buildings-highlight-shadow";
+
+console.log("const added");
--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-mapboxgl.accessToken = "pk.eyJ1IjoiZ2lzZmVlZGJhY2siLCJhIjoiY2pwdWJjaXRuMGV6YjQyb2c2NDY3b29wdCJ9.54kPmhZkN3sfNjDNUDqwIg";
 
-// We save layer IDs here so we can refer to them later
-var buildingsLayerID = "dam-buildings";
-var highlightExtrusionLayerID = "dam-buildings-highlight-extrusion";
-var highlightShadowLayerD = "dam-buildings-highlight-shadow";
 
+console.log(buildingsLayerID);
 // This contains the application state.
 var app = {
     // When we have a popup, we'll store it here so that we can remove it
@@ -104,7 +100,6 @@ var app = {
 // Creates and returns the map.
 function initMap(views) {
 
-    console.log("This where it all begins!!");
     var bounds = [
         [144.88, -37.86], // SW
         [145.01, -37.76], // NE
